a0a6279c6e2990ddf4d184e17835b27f031dcadf
victor307 <victor@gridinc.com>
2022-05-13 14:20:54 +0900

    feat: k6 config 추가, run script 추가

--- api.test.ts
+++ api.test.ts
@@ -1,9 +1,14 @@
 import http from 'k6/http';
 import {check, sleep} from 'k6';
+import _ from 'https://cdn.jsdelivr.net/npm/underscore@1.13.3/underscore-umd-min.js';
 
 export default function() {
+
+  const arr = [3,6,9,1,13];
+  console.log('underscore first=', _.first(arr));
+
   const data = {username: 'username', password: 'password'};
   let res = http.get('http://localhost:3000/', data);
-    check(res, { 'success login': (r) => r.status === 200 });
-    sleep(0.3);
+  check(res, { 'success login': (r) => r.status === 200 });
+  sleep(0.3);
 }
